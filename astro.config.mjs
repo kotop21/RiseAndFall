9e27d881e9bbde04176e323fb072c341cbb3427f
Victor <72082776+kotop21@users.noreply.github.com>
2026-04-14 19:52:52 +0300

    Fix: project for deploy in github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://kotop21.github.io',
+  base: '/название_репозитория',
   build: {
     inlineStylesheets: 'never',
   },
