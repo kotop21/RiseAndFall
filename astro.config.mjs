3dc6a340640889ade5adf01342aad11237c2c68f
Victor <72082776+kotop21@users.noreply.github.com>
2026-04-16 19:36:17 +0300

    Add icon and seo file for website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://kotop21.github.io',
   base: '/RiseAndFall',
   trailingSlash: 'always',
+
   build: {
     inlineStylesheets: 'never'
-  }
+  },
+
+  integrations: [sitemap()]
 });
